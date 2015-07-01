1f2c4141f3b59b31c8b4472d01cf3501e689a274
nilac8991 <nilac8991@gmail.com>
2015-07-01 19:45:20 +0200

    Added ROM Part function
    Signed-off-by: nilac8991 <nilac8991@gmail.com>

--- main.sh
+++ main.sh
@@ -52,8 +52,60 @@ echo -e "	    	    \_____  \_/ ___\_  __ \  \____ \   __\ "
 echo -e "	    	    /        \  \___|  | \/  |  |_> >  |  "
 echo -e "	    	    /_______  /\___  >__|  |__|   __/|__|  "
 echo -e "	    	    	    \/     \/         |__| "
+echo -e ""
+if [[ -n $romchoose ]];then
+echo -e "							Script is running in a $romchoose sorce  dir	"
+else
+echo -e ""
+fi
 }
+
+	# Need here to include the control to see if the file exists in order to launch ROM or not
+	# For now ROM will be launched always
 
 #while [[ true ]]; do
 	KITCHENSPLASH
 #done
+
+function ROM () {
+
+
+		echo -e "Welcome! the script detected that it isn't situated in any ROM sources dir"
+		echo -e "So in order you will need to choose one of the supported ROMs by the script:"
+		echo -e "CyanogenMod"
+		echo -e "SlimRoms"
+		echo -e "BlissPop"
+		echo -e "CarbonROM"
+		echo -e "Please insert the ROM name as is stated here or the script won't accept it.."
+		read romchoose
+
+			if [[ $romchoose = "CyanogenMod" || $romchoose = "BlissPop" || $romchoose = "SlimRoms" || $romchoose = "CarbonROM" ]]; then
+				echo -e "You choosed $romchoose"
+				echo -e "Going to redirect you to the Repo initialization part in now time.."
+				sleep 2
+				#REPO INIT function
+
+			else
+
+				echo -e "Wrong ROM name! Please try again.."
+				sleep 2
+				echo -e "Choose the ROM name (As stated in the list):"
+				read romchoose
+					if [[ $romchoose = "CyanogenMod" || $romchoose = "BlissPop" || $romchoose = "SlimRoms" || $romchoose = "CarbonROM" ]]; then
+							echo -e "You choosed $romchoose"
+							echo -e "Going to redirect you to the Repo initialization part in now time.."
+							#REPO INIT function
+					else
+							echo -e "You still picked up a wrong ROM name..."
+							echo -e "You want to exit?"
+							read askexit
+							if [[ $exit = "Yes" || $exit = "yes" || $exit = "YES" ]]; then
+								exit 0
+							else
+							echo -e "ROM function going to be launched again..."
+							fi
+					fi
+			fi
+}
+
+ROM
